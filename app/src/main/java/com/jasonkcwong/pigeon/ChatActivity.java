9b367c32e4db8f6f4105b4cb5057965e697dd616
Jason Wong <jason_19960903@hotmail.com>
2016-08-01 20:36:27 -0700

    Build UI for chat

--- app/src/main/java/com/jasonkcwong/pigeon/ChatActivity.java
+++ app/src/main/java/com/jasonkcwong/pigeon/ChatActivity.java
@@ -6,6 +6,10 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import com.jasonkcwong.pigeon.Fragment.ContactsFragment;
 import com.jasonkcwong.pigeon.Models.Contact;
@@ -17,17 +21,32 @@ import com.jasonkcwong.pigeon.Models.Contact;
 public class ChatActivity extends AppCompatActivity{
     public static final String TAG = ChatActivity.class.getName();
     private ActionBar mActionBar;
+    private EditText mMessageText;
+    private Button mSubmitButton;
+    private ListView mChatListView;
     private Contact contact;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
         mActionBar = getSupportActionBar();
         mActionBar.setDisplayHomeAsUpEnabled(true);
         mActionBar.setDisplayShowHomeEnabled(true);
         contact = (Contact) getIntent().getSerializableExtra(ContactsFragment.EXTRA_CONTACT);
         mActionBar.setTitle(contact.getDisplayName());
-    }
+
+        mMessageText = (EditText) findViewById(R.id.edit_message);
+        mSubmitButton = (Button) findViewById(R.id.button_submit);
+        mChatListView = (ListView) findViewById(R.id.chat_list);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
